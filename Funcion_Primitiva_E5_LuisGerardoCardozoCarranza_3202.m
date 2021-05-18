975b5d295d02220f1d0daf0d1d1e41a220dfeccc
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-18 17:17:47 -0500

    Update Funcion_Primitiva_E5_LuisGerardoCardozoCarranza_3202.m

--- Funcion_Primitiva_E5_LuisGerardoCardozoCarranza_3202.m
+++ Funcion_Primitiva_E5_LuisGerardoCardozoCarranza_3202.m
@@ -14,7 +14,7 @@ pkg load symbolic
 %El comando sym nos permite crear y dar un valor inicial a una variable simbólica.
 syms x;
 %La integracion se realizara respecto a la variable simbolica por defecto. 
-int(2(e.^x)+(4^x))
+int(2(e.^x)+(4.^x))
 %La respuesta de la operacion no ha sido asignado a ninguna variable y por defecto es ans
 ans = (sym)
-AVISO: Syntax ERROR!
+
