74d9134147b04f166b7b132a7fd879d54f97b596
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-17 19:26:47 -0500

    Create Funcion_Primitiva_E13_LuisGerardoCardozoCarranza_3202.m

---
+++ Funcion_Primitiva_E13_LuisGerardoCardozoCarranza_3202.m
@@ -0,0 +1,18 @@
+% Octave Script
+% Title			:1.7 Funcion Primitiva
+% Description		:Script para graficar el ejericio 13 de la actividad 1.7
+% Author		:Luis Gerardo Cardozo Carranza (Gerard_CRS) luiscardozo161002@gmail.com
+% Date			:20210517
+% sion		        :1
+% Usage			:octave> /path/Funcion_Primitiva_Actividad_1.7_Luis Gerardo Cardozo Carranza_3202
+% Notes			:Requiere aplicacion octave usar en consola preferentemente.
+
+%Actividad_Ejercicio 13 ∫ 3x^4 dx
+%Carga de la paqueteria Symbolic
+pkg load symbolic
+%El comando sym nos permite crear y dar un valor inicial a una variable simbólica.
+syms x;
+%La integracion se realizara respecto a la variable simbolica por defecto. 
+int 3(x.^4) 
+%La respuesta de la operacion no ha sido asignado a ninguna variable y por defecto es ans
+ans = (sym)
