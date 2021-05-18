4ba2b2eb1224a7c8638b2ca07acc78ad739a610c
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-17 20:40:39 -0500

    Update Funcion_Primitiva_E19_LuisGerardoCardozoCarranza_3202.m

--- Funcion_Primitiva_E19_LuisGerardoCardozoCarranza_3202.m
+++ Funcion_Primitiva_E19_LuisGerardoCardozoCarranza_3202.m
@@ -6,10 +6,45 @@
 % sion		        :1
 % Usage			:octave> /path/Funcion_Primitiva_Actividad_1.7_Luis Gerardo Cardozo Carranza_3202
 % Notes			:Requiere aplicacion octave usar en consola preferentemente.
-                        4
+%                       4
 %Actividad_Ejercicio 19 ∫ x dx
-                        1
-%Definir la funcion  
-f=@(x,y) x;
-res=integral(f,1,4);
-fprintf('El valor de la integral es: %2.1f\n',res)
+%                       1
+%Limpia la pantalla y las variables.
+clc, clear
+
+%Define los intervalos y el numero de rectangulos.
+a = 1;
+b = 4;
+n = 100;
+
+%Determina la longitud de la base o el incremento de x.
+base = (b-a)/n;
+
+%Definicion de la funcion.
+x =a:base:(b-base);
+
+%Y esta representada por la variable altura
+altura=(x);
+
+%Establece   la    linea   que   forma  la funcion
+x2 = linspace(1,4,100);
+y2 = (x);
+
+%Inicio del  intervalo, fin del intervalo, numero de rectangulos.
+xa = linspace(1,4,100);
+yab =(x);
+
+%Dibuja una  grafica de barras una a lado de otra sin espacios
+g = bar(xa,yab,'histc');
+
+%Permite continuar  graficando  despues de que ya exista un grafica trazada
+hold on
+
+%Grafica la funcion
+plot(x2,y2);
+
+%Asigna un nombre a la grafica 
+title("Actividad Ejercicio 19 ");
+
+%Ajusta la grafica a los datos.
+axis tight
