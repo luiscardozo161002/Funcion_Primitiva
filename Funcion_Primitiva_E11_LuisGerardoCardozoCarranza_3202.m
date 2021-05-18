aa2cdb50672a09c29e151105ba24dddb938b1dfc
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-18 17:57:15 -0500

    Update Funcion_Primitiva_E11_LuisGerardoCardozoCarranza_3202.m

--- Funcion_Primitiva_E11_LuisGerardoCardozoCarranza_3202.m
+++ Funcion_Primitiva_E11_LuisGerardoCardozoCarranza_3202.m
@@ -13,6 +13,6 @@ pkg load symbolic
 %El comando sym nos permite crear y dar un valor inicial a una variable simbólica.
 syms x;
 %La integracion se realizara respecto a la variable simbolica por defecto. 
-int sqrt(x) 
+int (sqrt(x)) 
 %La respuesta de la operacion no ha sido asignado a ninguna variable y por defecto es ans
 ans = (sym)
