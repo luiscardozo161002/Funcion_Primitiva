02a01566c9c6772ee4303da42f81c2793a78b5e0
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-19 13:47:40 -0500

    Update Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m

--- Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m
+++ Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m
@@ -6,30 +6,48 @@
 % sion		        :1
 % Usage			:octave> /path/Funcion_Primitiva_Actividad_1.7_Luis Gerardo Cardozo Carranza_3202
 % Notes			:Requiere aplicacion octave usar en consola preferentemente.
-%                       4
-%Actividad_Ejercicio 22 ∫ (2x^2-3x) dx
-%                      -1
-%Limpia la pantalla y las variables.
+%                       3
+%Actividad_Ejercicio 4  ∫ (2x^2-3x) dx
+%                      -3
+
+%limpia la pantalla y las variables.
 clc, clear
-%Definimos las variables 
-f=@(x,y) (2*x.^2-3*x);
-%Declaramos la variable para la funcion
-resultado=integral(f,-3,3);
-%imprimimos el valor de la integral definida
-fprintf('El valor de la integral es:%2.1f\n',resultado)
-%De nuevo tomamos los valores para definir el rango de la grafica
-x=[-3:0.1:3];
 %Definimos la funcion
-y=(2*x.^2)-(3*x);
-%Imprimimos la funcion en los ejes x,y
-plot(x,y)
-%Muestra las líneas de cuadrícula principales para los ejes 
-grid on
-%Permite continuar  graficando  despues de que ya exista un grafica trazada
+f=@(x,y) (2*x.^2-3*x);
+%Declaramos los intervalos
+resultado=integral(f, -3,3);
+%Imprimos en pantalla el resultado de la Integral
+fprintf('El valor de la integral es:%2.1f\n',resultado)
+%define los intervalos y el numero de rectangulos.
+a = -3;
+b =  3;
+n = 100;
+%Determina la longitud de la base o el incremento de x.
+base = (b-a)/n;
+%Definicion de la funcion.
+x =a:base:(b-base);
+%Representa la variable altura
+altura=(2*x.^2)-(3*x);
+%Calcula el valor de cada uno de los rectangulos.
+area = base*altura;
+%Estable    la    linea   que   forma  la funcion
+x2 = linspace(-3,3,100);
+y2 = (2*x.^2)-(3*x);
+%Inicio del  intervalo, fin del intervalo, numero de rectangulos.
+xa = linspace(-3,3,100);
+yab= (2*x.^2)-(3*x);
+
+%Dibuja una  grafica de barras sin espacios
+g = bar(xa,yab,'histc');
+
+%Permite continuar graficando despues de la grafica
 hold on
+
 %Grafica la funcion
-plot(x,y)
-%distribuye las barras a lo largo del eje x
-bar(x,y)
-%Titulo de la funcion
-title("Ejercicio 4 Integral Definida");
+plot(x2,y2);
+
+%Asigna un nombre a la grafica 
+title("Ejericicio 4 Integrales Definidas");
+
+%Ajusta la grafica a los datos.
+axis tight
