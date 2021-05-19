c81c4fd5711e2be357326206768e8dda0eae6cfc
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-19 09:06:59 -0500

    Update Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m

--- Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m
+++ Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m
@@ -9,6 +9,42 @@
 %                       3
 %Actividad_Ejercicio 22 ∫ (2x^2-3x) dx
 %                      -3
-fun=@(x) 2(x.^2-3*x);
+fun=@(x,y) (2*x.^2-3*x);
 resultado=integral(f,-3,3);
 fprintf('El valor de la integral es: %2.1f\n',resultado)
+%define los intervalos y el numero de rectangulos.
+a =  -3;
+b =   3;
+n = 100;
+%determina la longitud de la base o el incremento de x.
+base = (b-a)/n;
+%definicion de la funcion.
+x =a:base:(b-base);
+%y esta representada por la variable altura
+altura=(2*x.^2)-(3*x);
+%calcula el valor de cada uno de los rectangulos.
+area = base*altura;
+%estable    la    linea   que   forma  la funcion
+x2 = linspace( 1,4,100);
+y2 = (2*x.^2)-(3*x);
+%inicio del  intervalo, fin del intervalo, numero 
+%de rectangulos.
+xa = linspace(1,4,100);
+yab =(2*x.^2)-(3*x);
+
+%dibuja una  grafica de barras una a lado de otra 
+%sin espacios
+g = bar(xa,yab,'histc');
+
+%permite continuar  graficando  despues de que ya 
+%exista un grafica trazada
+hold on
+
+%Grafica la funcion
+plot(x2,y2);
+
+%Asigna un nombre a la grafica 
+title("Ejericicio 4 Integrales Definidas");
+
+%ajusta la grafica a los datos.
+axis tight
