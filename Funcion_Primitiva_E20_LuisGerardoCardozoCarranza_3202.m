a59b2b9073d4f0053ac41594098237b41d3fd120
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-18 20:21:25 -0500

    Update Funcion_Primitiva_E20_LuisGerardoCardozoCarranza_3202.m

--- Funcion_Primitiva_E20_LuisGerardoCardozoCarranza_3202.m
+++ Funcion_Primitiva_E20_LuisGerardoCardozoCarranza_3202.m
@@ -12,5 +12,42 @@
 %Limpia la pantalla y las variables.
 clc, clear
 f=@(x,y)x;
-resultado=integral(f,1,4);
-fprintf('El velor de la integral es:%2.1f\n',resultado)
+resultado=integral(f, -1,4);
+fprintf('El valor de la integral es:%2.1f\n',resultado)
+
+%Define los intervalos y el numero de rectangulos.
+a = -1;
+b =  4;
+n = 100;
+
+%Determina la longitud de la base o el incremento de x.
+base = (b-a)/n;
+
+%Definicion de la funcion.
+x =a:base:(b-base);
+
+%Y esta representada por la variable altura
+altura=(x);
+
+%Establece   la    linea   que   forma  la funcion
+x2 = linspace(1,4,100);
+y2 = (x);
+
+%Inicio del  intervalo, fin del intervalo, numero de rectangulos.
+xa = linspace(1,4,100);
+yab =(x);
+
+%Dibuja una  grafica de barras una a lado de otra sin espacios
+g = bar(xa,yab,'histc');
+
+%Permite continuar  graficando  despues de que ya exista un grafica trazada
+hold on
+
+%Grafica la funcion
+plot(x2,y2);
+
+%Asigna un nombre a la grafica 
+title("Ejercicio 2 Integral Definida");
+
+%Ajusta la grafica a los datos.
+axis tight
