7b519907c9e47d2bfd30513627bc72cd68dbbfe7
luiscardozo161002 <78442724+luiscardozo161002@users.noreply.github.com>
2021-05-19 13:17:48 -0500

    Update Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m

--- Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m
+++ Funcion_Primitiva_E22_LuisGerardoCardozoCarranza_3202.m
@@ -6,45 +6,30 @@
 % sion		        :1
 % Usage			:octave> /path/Funcion_Primitiva_Actividad_1.7_Luis Gerardo Cardozo Carranza_3202
 % Notes			:Requiere aplicacion octave usar en consola preferentemente.
-%                       3
+%                       4
 %Actividad_Ejercicio 22 ∫ (2x^2-3x) dx
-%                      -3
-fun=@(x,y) (2*x.^2-3*x);
+%                      -1
+%Limpia la pantalla y las variables.
+clc, clear
+%Definimos las variables 
+f=@(x,y) (2*x.^2-3*x);
+%Declaramos la variable para la funcion
 resultado=integral(f,-3,3);
-fprintf('El valor de la integral es: %2.1f\n',resultado)
-%define los intervalos y el numero de rectangulos.
-a =  -3;
-b =   3;
-n = 100;
-%determina la longitud de la base o el incremento de x.
-base = (b-a)/n;
-%definicion de la funcion.
-x =a:base:(b-base);
-%y esta representada por la variable altura
-altura=(2*x.^2)-(3*x);
-%calcula el valor de cada uno de los rectangulos.
-area = base*altura;
-%estable    la    linea   que   forma  la funcion
-x2 = linspace( 1,4,100);
-y2 = (2*x.^2)-(3*x);
-%inicio del  intervalo, fin del intervalo, numero 
-%de rectangulos.
-xa = linspace(1,4,100);
-yab =(2*x.^2)-(3*x);
-
-%dibuja una  grafica de barras una a lado de otra 
-%sin espacios
-g = bar(xa,yab,'histc');
-
-%permite continuar  graficando  despues de que ya 
-%exista un grafica trazada
+%imprimimos el valor de la integral definida
+fprintf('El valor de la integral es:%2.1f\n',resultado)
+%De nuevo tomamos los valores para definir el rango de la grafica
+x=[-3:0.1:3];
+%Definimos la funcion
+y=(2*x.^2)-(3*x);
+%Imprimimos la funcion en los ejes x,y
+plot(x,y)
+%Muestra las líneas de cuadrícula principales para los ejes 
+grid on
+%Permite continuar  graficando  despues de que ya exista un grafica trazada
 hold on
-
 %Grafica la funcion
-plot(x2,y2);
-
-%Asigna un nombre a la grafica 
-title("Ejericicio 4 Integrales Definidas");
-
-%ajusta la grafica a los datos.
-axis tight
+plot(x,y)
+%distribuye las barras a lo largo del eje x
+bar(x,y)
+%Titulo de la funcion
+title("Ejercicio 4 Integral Definida");
